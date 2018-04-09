dc3a136dc514fe1209e8d7b3b3c04fc2c6256cb7
David Emily <daeth5@mail.missouri.edu>
2018-04-08 23:05:46 -0500

    Update user.py

--- user.py
+++ user.py
@@ -5,9 +5,8 @@ from flask_httpauth import SQLAlchemy
 
 class User(db.Model):
   __tablename__ = 'Users'
-  id = db.Column(db.Integer, primary_key = True)
   firstName = db.Column(db.String(255))
   lastName = db.Column(db.String(255))
   authType = db.Column(db.String(255))
   email = db.Column(db.String(255))
-  
+  id = db.Column(db.Integer, primary_key = True)  
