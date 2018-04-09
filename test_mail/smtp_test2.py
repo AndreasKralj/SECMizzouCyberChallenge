3d473eb280c973d4164aba409c44303449f494a8
Ubuntu <ubuntu@ip-172-31-14-173.us-east-2.compute.internal>
2018-04-09 01:08:17 +0000

    Finished email function

--- test_mail/smtp_test2.py
+++ test_mail/smtp_test2.py
@@ -2,37 +2,51 @@
 
 import smtplib
 
-gmail_user = 'AnfreasKralj@gmail.com'  
-gmail_password = 'pw' #It's kind of shady to me that you need to put the password in the code, but it works.
+def SendSSLEmail(email_address, body):
+    if not body:
+        body = "Specific error message not specified."
+        
+    if not email_address:
+        print "No email address specified, sending to default user"
+        body += "\n\nNote: The email address was not specified, so it was sent to the default authority.\n"
+        #Set a default email address for the application.
+        email = "ankwdf@mail.missouri.edu"
+    
+    gmail_user = 'AnfreasKralj@gmail.com'  
+    gmail_password = '89ece8b871' #It's kind of shady to me that you need to put the password in the code, but it works.
 
-sent_from = gmail_user  
-to = ['ankwdf@mail.missouri.edu']  
-subject = 'Test email'
-body = "Hey, what's up?\n\n- You"
+    sent_from = gmail_user  
+    to = ['ankwdf@mail.missouri.edu']  
+    subject = 'Test email'
+    #body = "Hey, what's up?\n\n- You"
 
-email_text = """\  
-From: %s  
-To: %s  
-Subject: %s
+    email_text = """  
+    From: %s  
+    To: %s  
+    Subject: %s
 
-%s
-""" % (sent_from, ", ".join(to), subject, body)
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
 
-#Attempt to send the email
-try:  
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    print "Got here!"
-    server.ehlo()
-    print "Got here!"
-    #This is where the error occurs.
-    server.starttls()
-    print gmail_user
-    print gmail_password
-    server.login(gmail_user, gmail_password)
-    print "Got here!"
-    server.sendmail(sent_from, to, email_text)
-    server.close()
+    #Attempt to send the email
+    try:  
+        server_SSL = smtplib.SMTP_SSL('smtp.gmail.com:465')
+        print "Got here!"
+        server_SSL.ehlo()
+        print "Got here!"
+        server_SSL.login(gmail_user, gmail_password)
+        print "Got here!"
+        server_SSL.sendmail(sent_from, to, email_text)
+        server_SSL.close()
 
-    print 'Email sent!'
-except:  
-    print 'Something went wrong...'
+        print 'Email sent!'
+    except:  
+        print 'Something went wrong...'
+    return;
+
+print "Successful email sent:"
+SendSSLEmail("ankwdf@mail.missouri.edu", "This is a test!")
+print "Missing email address:"
+SendSSLEmail("", "This is a test!")
+print "Missing body of email"
+SendSSLEmail("ankwdf@mail.missouri.edu", "")
