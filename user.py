fd178d53ba36baaada59252303b0e3637a766d9f
David Emily <daeth5@mail.missouri.edu>
2018-04-08 22:51:34 -0500

    Create user.py

---
+++ user.py
@@ -0,0 +1,13 @@
+import app
+import os
+from flask_sqalchemy import SQLAlchemy
+from flask_httpauth import SQLAlchemy
+
+class User(db.Model):
+  __tablename__ = 'Users'
+  id = db.Column(db.Integer, primary_key = True)
+  firstName = db.Column(db.String(255))
+  lastName = db.Column(db.String(255))
+  authType = db.Column(db.String(255))
+  email = db.Column(db.String(255))
+  
